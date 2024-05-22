e6a0bd9282fe50d190f6c24a231c607ea7d5816e
Martin Silva <martinalejandrosilva@gmail.com>
2024-05-21 21:57:56 -0300

    Install Paper

--- Movieplay/App.tsx
+++ Movieplay/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './assets/localization/i18n';
 import {Provider} from 'react-redux';
 import store from './store/store';
+import {Button, PaperProvider} from 'react-native-paper';
 
 function App(): React.JSX.Element {
   return (
     <Provider store={store}>
-      <></>
+      <PaperProvider>
+        <Button>Press Here!</Button>
+      </PaperProvider>
     </Provider>
   );
 }
