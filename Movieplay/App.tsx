a33530ed4589f58d57f51deda415154b3246d6bb
Martin Silva <martinalejandrosilva@gmail.com>
2024-05-29 21:35:40 -0300

    Martin Changes

--- Movieplay/App.tsx
+++ Movieplay/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import Splash from './src/auth/Splash'; //<Splash/>
-import Login from './src/auth/Splash/login'; //<Login/>
+import './assets/localization/i18n'; //Do not remove.
+
+import {Provider} from 'react-redux';
+import store from './src/store/store';
+import {PaperProvider} from 'react-native-paper';
+import RootStackNavigation from './src/navigation/RootNavigation';
+
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-//const Stack = createNativeStackNavigator();
+import {enableScreens} from 'react-native-screens';
+
+enableScreens();
 
 export default function App() {
   return (
-    <View>
-      <Login />
-    </View>
+    <Provider store={store}>
+      <PaperProvider>
+        <NavigationContainer>
+          <RootStackNavigation />
+        </NavigationContainer>
+      </PaperProvider>
+    </Provider>
   );
 }
 
--- Movieplay/src/navigation/RootNavigation.tsx
+++ Movieplay/src/navigation/RootNavigation.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import tabNavigator from './tabNavigator';
+import Login from '../auth/Splash/login';
 
 export type RootStackNavigationParams = {
-  Landing: undefined;
-  TabNavigator: undefined;
+  Login: undefined;
+  //TabNavigator: undefined;
   //Include here other screens
 };
 
@@ -13,12 +13,16 @@ const Stack = createNativeStackNavigator<RootStackNavigationParams>();
 const RootStackNavigation = () => {
   return (
     <Stack.Navigator>
-      {/* <Stack.Screen name="Landing" component={Landing} /> */}
       <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{headerShown: false}}
+      />
+      {/* <Stack.Screen
         name="TabNavigator"
         component={tabNavigator}
         options={{headerShown: false}}
-      />
+      /> */}
     </Stack.Navigator>
   );
 };
