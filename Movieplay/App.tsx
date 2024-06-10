37cbfd2817c26881a95523a29cea7eea39525558
augustom1995 <mancuello95augusto@hotmail.com>
2024-06-10 01:27:27 -0300

    Splash updated

--- Movieplay/App.tsx
+++ Movieplay/App.tsx
@@ -1,19 +1,58 @@
-import React from "react";
-import { View, Text } from "react-native";
-import Splash from './src/auth/Splash' //<Splash/>
+/*
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
+import Splash from './src/auth/Splash';
 import Login from './src/auth/Splash/login'//<Login/>
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-//const Stack = createNativeStackNavigator();
 
-export default function App(){
-  return(
-    
-    <View>
+export default function App() {
+  const [currentScreen, setCurrentScreen] = useState('Splash');
 
-      <Login/>
-      
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCurrentScreen('Login');
+    }, 5000); // 5000ms = 5 segundos
+
+    return () => clearTimeout(timer); // Limpia el temporizador en el desmontaje
+  }, []);
+
+  let screen;
+  if (currentScreen === 'Splash') {
+    screen = <Splash />;
+  } else if (currentScreen === 'Login') {
+    screen = <Login />;
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      {screen}
     </View>
   );
 }
 
+*/
+
+import React, { useState } from "react";
+import { View } from "react-native";
+import Splash from './src/auth/Splash';
+import Login from './src/auth/Splash/login'//<Login/>
+
+export default function App() {
+  const [currentScreen, setCurrentScreen] = useState('Splash');
+
+  const switchScreen = (screen: React.SetStateAction<string>) => {
+    setCurrentScreen(screen);
+  };
+
+  let screen;
+  if (currentScreen === 'Splash') {
+    screen = <Splash switchScreen={switchScreen} />;
+  } else if (currentScreen === 'Login') {
+    screen = <Login />;
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      {screen}
+    </View>
+  );
+}
