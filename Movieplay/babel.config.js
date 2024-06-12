9926aab629d78f2e8a9e92ddfc763f22e749d62c
Martin Alejandro Silva <martsilva@uade.edu.ar>
2024-06-11 23:19:35 -0300

    Martin changes + augusto login (#7)
    * Splash + Login front
    
    * Login updated
    
    * Splash updated
    
    * More changes
    
    ---------
    
    Co-authored-by: augustom1995 <mancuello95augusto@hotmail.com>
    Co-authored-by: Martin Silva <martinalejandrosilva@gmail.com>

--- Movieplay/babel.config.js
+++ Movieplay/babel.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['@babel/plugin-transform-class-properties', {loose: true}],
+    ['@babel/plugin-transform-private-methods', {loose: true}],
+    ['@babel/plugin-transform-private-property-in-object', {loose: true}],
+  ],
   env: {
     production: {
-      plugins: ['react-native-paper/babel'],
+      plugins: [
+        'react-native-paper/babel',
+        ['@babel/plugin-transform-class-properties', {loose: true}],
+        ['@babel/plugin-transform-private-methods', {loose: true}],
+        ['@babel/plugin-transform-private-property-in-object', {loose: true}],
+      ],
     },
   },
 };
