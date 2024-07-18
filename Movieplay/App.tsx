3b80bf3f0456967ae7ce7ae3eb3ecc5ae6763848
Commondevelopernp <56087940+commondevelopernp@users.noreply.github.com>
2024-07-18 12:25:43 -0300

    Update App.tsx

--- Movieplay/App.tsx
+++ Movieplay/App.tsx
@@ -73,6 +73,7 @@ export default function App() {
           const decodedToken = jwtDecode(token); // Decodificar el token JWT
           setIsLoggedIn(true);
           setUserClaims(decodedToken); // Almacenar las claims del usuario en el estado
+          console.log(userClaims);
         } else {
           setIsLoggedIn(false);
         }
